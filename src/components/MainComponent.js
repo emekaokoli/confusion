9bafe86102ec27589de219199c6aad4a1a4d1658
Emeka Okoli <emekaokoli00@gmail.com>
2020-06-16 14:17:33 +0100

    Single Page Applications Part 1.Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,10 @@
 import { Component, default as React } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
@@ -9,37 +13,40 @@ import Menu from './MenuComponent';
 
 export default class Main extends Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish:null
-    }
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
   }
 
-  onDishSelected(dishId){
-    this.setState({selectedDish:dishId})
-  }
-
+  // onDishSelected(dishId){
+  //   this.setState({selectedDish:dishId})
+  // }
   
   render() {
-    const HomePage = () => {
-      return <Home />
-    }
+     const HomePage = () => {
+       return (
+         <Home
+           dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+           promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+           leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+         />
+       )
+     }
     return (
       <>
-      <Header/>
-
+        <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
-          <Route
-            exact
-            path='/menu'
-            component={() => <Menu dishes={this.state.dishes} />}
-          />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
-        <Footer/>
+        <Footer />
       </>
     )
   }
